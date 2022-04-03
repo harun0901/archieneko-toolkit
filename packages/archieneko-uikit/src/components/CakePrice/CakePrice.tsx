8b14c55ed83fe5ee99b94fd2c19f069cfb766ca9
lincheng <lincheng930901@gmail.com>
2022-04-04 02:33:22 +0900

    first version of toolkit

---
+++ packages/archieneko-uikit/src/components/CakePrice/CakePrice.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { PancakeRoundIcon } from "../Svg";
+import Text from "../Text/Text";
+import Skeleton from "../Skeleton/Skeleton";
+import { Colors } from "../../theme";
+
+export interface Props {
+  color?: keyof Colors;
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
+  return cakePriceUsd ? (
+    <PriceLink
+      href="https://app.uniswap.org/#/swap?outputCurrency=0xFE5F69dfa2d4501E78078266F6d430c079098f90"
+      target="_blank"
+    >
+      <PancakeRoundIcon width="24px" mr="8px" />
+      <Text color={color} bold>{`$${cakePriceUsd.toFixed(19)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
